0152025203054fc9064affb8916c72d14f5e9872
nghia <minhnghia191189@gmail.com>
2022-12-24 13:09:14 +0700

    nghiadm first commit

---
+++ sprint1/src/main/java/com/c0821g1/sprint1/entity/NghiaDM.java
@@ -0,0 +1,24 @@
+package com.c0821g1.sprint1.entity;
+
+public class NghiaDM {
+    private int number;
+    private Long numberTwo;
+     public int getNumber(){
+         return number;
+     }
+     public void setNumber(int number){
+         this.number = number;
+     }
+
+    public Long getNumberTwo() {
+        return numberTwo;
+    }
+
+    public void setNumberTwo(Long numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("nghiadm1");
+    }
+}
