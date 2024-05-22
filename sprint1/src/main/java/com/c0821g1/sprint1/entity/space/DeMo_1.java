e0f4fe886428f2591e50b8fa1f0d899a6b0d25ff
nghia <minhnghia191189@gmail.com>
2024-05-22 10:53:53 +0700

    main commit 1

--- sprint1/src/main/java/com/c0821g1/sprint1/entity/space/DeMo_1.java
+++ sprint1/src/main/java/com/c0821g1/sprint1/entity/space/DeMo_1.java
@@ -1,7 +1,6 @@
 package com.c0821g1.sprint1.entity.space;
 
 public class DeMo_1 {
-<<<<<<< HEAD
     public static void main(String[] args) {
 
         System.out.println("demo motnnnnnnnnn");
@@ -15,8 +14,6 @@ public class DeMo_1 {
     public static void abc(){
         System.out.println("abc");
     }
-=======
     private String name;
     
->>>>>>> NghiaDM
 }
